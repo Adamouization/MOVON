4e6d67b66344b79c6492d99a425c65a3ad9016b5
Adamouization <aj645@bath.ac.uk>
2017-03-25 23:30:04 +0000

    terms and privacy page

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Launchpad!' });
+  res.render('index', { title: 'MOVON' });
 });
 
 /* GET sports gallery page. */
 router.get('/sports', function(req, res, next) {
     res.render('sports', { title: 'Sports selection' });
+});
+
+/* GET sports gallery page. */
+router.get('/terms-privacy', function(req, res, next) {
+    res.render('misc/terms-privacy', { title: 'Terms & Privacy' });
 });
 
 module.exports = router;
