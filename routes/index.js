8ca06aab21a8c3b7053ce2517b9e5ce5b1d56d70
Adamouization <aj645@bath.ac.uk>
2017-03-25 23:05:20 +0000

    code reorganisation + sports gallery basic html

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Launchpad!' });
 });
 
+/* GET sports gallery page. */
+router.get('/sports', function(req, res, next) {
+    res.render('sports', { title: 'Sports selection' });
+});
+
 module.exports = router;
