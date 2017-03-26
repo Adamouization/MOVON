5398d38494c80dc53eace43372d79f71436a1ca6
SamLubbers <lubbers.sc@gmail.com>
2017-03-26 10:26:41 +0100

    BUDFBGD

--- public/script/functionality.js
+++ public/script/functionality.js
@@ -5,4 +5,16 @@ $(document).ready(function(){
     $(".btn-xs").click(function () {
 
     });
+    $("#changeDate1").click(function () {
+        $("#date").html('Monday 27th of March');
+    });
+    $("#changeDate2").click(function () {
+        $("#date").html('Tuesday 28th of March');
+    });
+    $("#changeDate3").click(function () {
+        $("#date").html('Wednesday 29th of March');
+    });
+    $("#changeDate4").click(function () {
+        $("#date").html('Thursday 30th of March');
+    });
 });
